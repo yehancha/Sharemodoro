c953499f4af0eb91edf979f00291f5fc4bd9750a
Yehan Pemarathne <yehancha@gmail.com>
2016-06-07 18:26:31 +0530

    Add basic login

---
+++ firebase.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCZZyvWIrwILpG3wgJ2na91wE1QfPZtb0U",
+    authDomain: "sharemodoro.firebaseapp.com",
+    databaseURL: "https://sharemodoro.firebaseio.com",
+    storageBucket: "sharemodoro.appspot.com",
+};
+firebase.initializeApp(config);
